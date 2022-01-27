27a3d8486e701fbe084b2dc53ac0d737f43713eb
FalseGod13 <1743796656@qq.com>
2022-01-27 23:38:52 +0800

    reset for origin after renaming username

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"learn/demos/socket/tcp"
 	"learn/tools/console"
 )
 
 func main() {
 	console.Clear()
-	//a
+	fmt.Println("it works, git remote remove origin, then git remote add oringin xxx")
 	tcp.RunTcp()
 }
