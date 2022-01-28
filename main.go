590c3dff940176f03526d376c567e722273d2384
v_peterlei~ <v_peterlei@tencent.com>
2022-01-28 09:48:26 +0800

    develop host: done reset git remote origin

--- main.go
+++ main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"learn/demos/socket/tcp"
 	"learn/tools/console"
 )
 
 func main() {
 	console.Clear()
-	fmt.Println("it works, git remote remove origin, then git remote add oringin xxx")
 	tcp.RunTcp()
 }
